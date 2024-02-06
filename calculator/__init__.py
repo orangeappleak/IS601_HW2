928778544d907da98be76ee85e54f2356e987968
lamamomoman <redmaniac0510@gmail.com>
2024-02-06 11:24:59 -0500

    Finalized the project structure and made some test changes

---
+++ calculator/__init__.py
@@ -0,0 +1,8 @@
+'''Basic Calculator Functionality'''
+def add(arg_a,arg_b):
+    '''Addition Function'''
+    return arg_a + arg_b
+
+def subtract(arg_a,arg_b):
+    '''Subtraction Function'''
+    return arg_a - arg_b
