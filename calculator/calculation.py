cdfdca9c079031389ffb00f9d85678efd191da36
Karthik Appannagari <scarlettblur@scarlett-blur.mynetworksettings.com>
2024-02-21 19:04:51 -0500

    added test to operations.oy

--- calculator/calculation.py
+++ calculator/calculation.py
@@ -28,5 +28,5 @@ class Calculation:
         Calculates the result using the stored operation function.
         """
         if self.operation is None:
-            raise ValueError("Operation is not defined")
+            raise ZeroDivisionError("Operation is not defined")
         return self.operation(self.arg_a, self.arg_b)
